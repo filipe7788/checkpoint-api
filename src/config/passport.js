9d4a6995cb0a2e8af67f6763132ae332477a063b
DESKTOP-1PNMURG\Filipe <filipe.cruz.ribeiro@gmail.com>
2025-11-29 02:19:33 -0300

    fix passport

--- src/config/passport.js
+++ src/config/passport.js
@@ -18,7 +18,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: `${process.env.API_URL}/auth/google/callback`,
+      callbackURL: `${process.env.API_URL}/api/auth/google/callback`,
       scope: ['profile', 'email'],
     },
     (accessToken, refreshToken, profile, done) => {
@@ -33,7 +33,7 @@ passport.use(
     {
       clientID: process.env.APPLE_CLIENT_ID,
       teamID: process.env.APPLE_TEAM_ID,
-      callbackURL: `${process.env.API_URL}/auth/apple/callback`,
+      callbackURL: `${process.env.API_URL}/api/auth/apple/callback`,
       keyID: process.env.APPLE_KEY_ID,
       privateKeyString: process.env.APPLE_PRIVATE_KEY,
       scope: ['email', 'name'],
